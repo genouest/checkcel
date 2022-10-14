b7030d9cbe7b5ed1ee79695e6811954bbe2eef4a
mboudet <mateo.boudet@gmail.com>
2022-10-14 10:19:32 +0200

    Better empty management

--- checkcel/validators.py
+++ checkcel/validators.py
@@ -550,10 +550,13 @@ class UniqueValidator(Validator):
         if self.ignore_space:
             field = field.strip()
 
-        if not field and not self.empty_ok:
-            raise ValidationException(
-                "Field cannot be empty"
-            )
+        if not field:
+            if self.empty_ok:
+                return
+            else:
+                raise ValidationException(
+                    "Field cannot be empty"
+                )
 
         if self.unique_with and not self.unique_check:
             self._precheck_unique_with(row)
